39653427cb1c437ef3d8ab218e0faf7dd6449024
Chris Ouellette <chrisryanouellette@gmail.com>
2021-02-24 16:44:50 -0500

    Project Setup

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+	mode: 'production',
+	entry: path.join(__dirname, '/src/index.ts'),
+	output: {
+		filename: 'index.js',
+		path: path.join(__dirname, '/dist'),
+		library: 'AT',
+		libraryTarget: 'this',
+		libraryExport: 'default',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.ts'],
+	},
+	externals: {
+		'@airtable/blocks': '@airtable/blocks',
+	},
+	watch: true,
+}
