614afb7f13dbb69b8aade4698dffb3f85f4227f1
Chris Ouellette <chrisryanouellette@gmail.com>
2021-02-26 14:52:36 -0500

    Fix: Format date and export utils

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { records } from './records'
 import { remote } from './remote'
 import { tables } from './tables'
+import { utils } from './utils'
 
 export default {
 	...tables,
 	...records,
+	...utils,
 	remote: remote,
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,16 @@
 export function formatDate(date: Date): string {
 	const month = (date.getMonth() + 1).toString().padStart(2, '0')
-	const day = (date.getDate() + 1).toString().padStart(2, '0')
+	const day = date.getDate().toString().padStart(2, '0')
 	return `${month}/${day}/${date.getFullYear()}`
 }
 
 export function formatTime(date: Date): string {
-	const hour = date.getHours() > 12 ? date.getHours() - 12 : date.getHours()
+	const hour =
+		date.getHours() > 12
+			? date.getHours() - 12
+			: date.getHours() !== 0
+			? date.getHours()
+			: 12
 	const minute = date.getMinutes().toString().padStart(2, '0')
 	const abbr = date.getHours() < 12 ? 'AM' : 'PM'
 	return `${hour.toString().padStart(2, '0')}:${minute} ${abbr}`
@@ -13,4 +18,10 @@ export function formatTime(date: Date): string {
 
 export function format(date: Date): string {
 	return `${formatDate(date)} ${formatTime(date)}`
+}
+
+export const utils = {
+	formatDate,
+	formatTime,
+	format,
 }
